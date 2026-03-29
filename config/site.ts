2f79711a4e3a4d71da72fefb35bd6c7b01b2974f
Stas Senishchs <stassenishchs@inet.com>
2026-03-29 14:30:40 +0300

    fix: telegram

--- config/site.ts
+++ config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig: SiteConfig = {
   tagline:
     "Ремонт автомобілів будь-якої складності. Діагностика. Двигун. Ходова. Електрика.",
   phone: "+380 93 419 54 91",
-  telegram: "https://t.me/stn_motors",
+  telegram: "https://t.me/stn_club",
   address: "вулиця Відкрита, 21, Львів, Львівська область, 79000",
   googleMapsUrl:
     "https://www.google.com/maps/place/%D0%A1%D0%A2%D0%9E+STN+CLUB/@49.8252429,24.0120373,18.45z/data=!4m6!3m5!1s0x473ae722d54375b3:0x903051f648371041!8m2!3d49.8258591!4d24.0118969!16s%2Fg%2F11xmqtc965?entry=ttu",
